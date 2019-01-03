88ec17922b4b9cd13ff120b3b4e7c999c71971b6
Bob Wang <35225645+BobWangRobot@users.noreply.github.com>
2019-01-03 21:59:25 +0800

    add
    add some new file

--- retest.py
+++ retest.py
@@ -5,6 +5,7 @@ from libtbx import group_args
 import mmtbx.model
 from libtbx.utils import null_out
 
+ty = ['C', 'H', 'O']
 def get_model(pdb_file_name, cif_file_name):
     pdb_inp = iotbx.pdb.input(file_name=pdb_file_name)
     restraint_objects = None
@@ -18,7 +19,7 @@ def get_model(pdb_file_name, cif_file_name):
         log=null_out())
     return model
 
-def read(model):
+def read1(model):
     Rs = []
     R = 0
     hierarchy = model.get_hierarchy()
@@ -27,8 +28,32 @@ def read(model):
             R = a1.distance(a2)
             Rs.append(R)
             R = 0
-            print("kk")
             print(Rs)
+
+def read(model, ele_name):
+    Rjs = []
+    Rks = []
+    zeta = []
+    hierarchy = model.get_hierarchy()
+    for e in ele_name:
+        for a1 in hierarchy.atoms():
+            e1 = a1.element.upper()
+            while e1 == e:
+                print(e)
+                for a2 in hierarchy.atoms():
+                    e2 = a2.element.upper
+                    while a2 != a1:
+                        Rj = a1.distance(a2)
+                        Rjs.append(Rj)
+                        for a3 in hierarchy.atoms():
+                            while a3 != a2:
+                                Rk = a2.distance(a3)
+                                A = a1.angle(a2, a3, deg = True)
+                                Rks.append(Rk)
+                                zeta.append(A)
+    print(e, Rjs, Rks,zeta)
+    return e, Rjs, Rks, zeta
+
 
 
 def exercise():
@@ -36,7 +61,7 @@ def exercise():
     for (pdb_file_name, cif_file_name) in files:
         print(pdb_file_name, "-" * 50)
         model = get_model(pdb_file_name=pdb_file_name, cif_file_name=cif_file_name)
-        result = read(model=model)
+        result = read(model, ty)
 
 if __name__ == '__main__':
     exercise()
