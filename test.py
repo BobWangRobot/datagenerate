ad7bef088dc198e5f1039a8ab07534ed769e664d
Bob <Bob>
2019-03-09 21:01:18 +0800

    tidy

--- test.py
+++ test.py
@@ -1,79 +1,17 @@
-import math
 import iotbx
+from AEV import Rpart
 
-def read():
-    return 0
+rs_values = [ 0.900000,1.437500, 1.975000, 2.512500, 3.050000, 3.587500, 4.125000, 4.662500]
+ts_values = [0.392699, 1.178097, 1.963495, 2.748894]
+angular_rs_values = [0.900000, 2.200000]
+radial_cutoff = 5.2
+angular_cutoff = 3.5
+radial_nu = 8
+radial_angular_nu = 8
+angular_zeta = 8
+Rj = [2.1, 2.2, 2.5]
 
-def D3(i,j,k):
-    a = []
-    d = 1
-    for b in range(0,i):
-        tmp = []
-        for c in range(0,j):
-            app = []
-            for e in range(1,k):
-                app.append(d)
-                d = d + 1
-            tmp.append(app)
-        a.append(tmp)
-    print(a)
-    return a
+a = Rpart('acid060.pdb')
+print(a.Req(Rj))
 
-def D2(i,j):
-    a = []
-    d = 1
-    for b in range(0,i):
-        tmp = []
-        for b in range(0,j):
-            d = d + 1
-            tmp.append(d)
-        a.append(tmp)
-    print(a)
-    return a
 
-def cutf(Rij,Rc):
-    if Rc >= Rij:
-       Fc = 0.5*math.cos(math.pi*Rij/Rc)+0.5
-    else:
-       Fc = 0
-    return Fc
-
-def Rpart(j, Rc, Rj, Rs, n):
-    GmRs = []
-    for b in range(8):
-        GmR = 0
-        for a in range(0,j-1):
-            f = cutf(Rj[a], Rc)
-            GmR = GmR + math.exp(n * ((Rj[a] - Rs[b]) ** 2))*f
-            GmRs.append(GmR)
-    return GmRs
-
-def Apart(j, k, l, zeta, zetas, n, Rj, Rk, Rs, Rc):
-    GmAs = []
-    i = 0
-    fj = cutf(Rj, Rc)
-    fk = cutf(Rk, Rc)
-    for d in range(2):
-        for c in range(4):
-            GmA = 0
-            for a in range(0,j-1):
-                for b in range(0, k-1):
-                    GmA = GmA + (((1+math.cos(zeta[a][b] - zetas[c])) )** l[d]) * \
-                          math.exp(-n *( ((Rj + Rk) / 2 - Rs) ** 2)) * fj * fk
-            GmA = GmA + 2 ** (1-l[d])
-            GmAs.append(GmA)
-            i = i + 1
-    print(GmAs, i)
-    return GmAs
-
-i = int(input("inputi"))
-j = int(input("inputj"))
-a = D2(i, j)
-zetas = [1, 2, 3, 4]
-l = [0,1]
-Rj = zetas
-Rs = [1, 2, 3, 4, 5, 6, 7, 8]
-Gmr = Rpart(j, 4, Rj, Rs, 6)
-#Gma = Apart(i, j, l, a, zetas, 2, 4, 5, 3, 6)
-print('g', Gmr)
-print(a[1][2])
