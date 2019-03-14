14f9c6f7956c8fd76deb5e34f4ac62b11e0f7486
Bob <896092022@qq.com>
2019-03-15 01:08:02 +0800

    fixing_Apart

--- AEVclass.py
+++ AEVclass.py
@@ -3,6 +3,18 @@ import numpy
 import iotbx
 import math
 
+class radial_aev_class(dict):
+  def __repr__(self):
+    outl = 'AEV ...\n'
+    for key, item in self.items():
+      outl += '  %s :\n' % (key)
+      for e, vec in item.items():
+        outl += '     %s : ' % e
+        for v in vec:
+          outl += '%0.3f,' % v
+        outl += '\n'
+    return outl
+
 class AEV(object):
 
   def __init__(self, pdb_file_name):
@@ -22,11 +34,6 @@ class AEV(object):
 #generate a dictionary about all atomes
   def Atome_classify(self):
       atom_elements = {}
-      list1 = list()
-      for a in self.hierarchy.atoms():
-          e = a.element.upper().strip()
-          list1.append(e)
-      list1 = set(list1)
       for b in self.hierarchy.atoms():
         e = b.element.upper().strip()
         atom_elements.setdefault(e, [])
@@ -35,55 +42,67 @@ class AEV(object):
 
 
 
-
 class Rpart(AEV):
     def __init__(self, pdb_file_name):
-        super(Rpart, self).__init__(pdb_file_name)
+        self.pdb_inp = iotbx.pdb.input(file_name=pdb_file_name)
+        self.hierarchy = self.pdb_inp.construct_hierarchy()
         self.rs_values = [0.900000, 1.437500, 1.975000, 2.512500, 3.050000, 3.587500, 4.125000, 4.662500]
         self.Rj =[2.1, 2.2, 2.5]
         self.cutoff = 3.5
 
-
     def R_AEV(self):
-        AEVs = {}#It is a multiple dictionary
+        AEVs = radial_aev_class() 
         n = 4.0
-        for a, atom_list1 in self.Atome_classify().items():
-            AEVs.setdefault(a, {})
-            for Rs in self.rs_values:
-                for atom1 in atom_list1:
-                    for b, atom_list2 in self.Atome_classify().items():
-                        AEVs[a].setdefault(b, [])
-                        GmR = 0
-                        for atom2 in atom_list2:
+        for atom1 in self.hierarchy.atoms():
+            x = str(atom1.i_seq)
+            a = atom1.element.upper().strip()
+            AEVs.setdefault(a+x, {})
+            for b, atom2list in self.Atome_classify().items():
+                for Rs in self.rs_values:
+                    AEVs[a+x].setdefault(b, [])
+                    GmR = 0
+                    for atom2 in atom2list:
+                        if atom1 != atom2:
                             R = atom1.distance(atom2)
                             f = self.cutf(R)
                             GmR += math.exp(- n * ((R - Rs) ** 2)) * f
-                        AEVs[a][b].append(GmR)
+                    AEVs[a+x][b].append(GmR)
         return AEVs
+
 
 class Apart(AEV):
     def __init__(self, pdb_file_name):
-        super(Rpart, self).__init__(pdb_file_name)
+        self.pdb_inp = iotbx.pdb.input(file_name=pdb_file_name)
+        self.hierarchy = self.pdb_inp.construct_hierarchy()
         self.ts_values = [0.392699, 1.178097, 1.963495, 2.748894]
         self.angular_rs_values = [0.900000, 2.200000]
-        self.angular_cutoff = 3.5
+        self.cutoff = 3.5
         self.angular_zeta = 8
-        self.radial_cutoff = 5.2
 
-    def Aeq(self):
+    def A_AEV(self):
         l = 8.00
         n = 4.0
-        GmAs = []
-        for a in self.angular_rs_values:
-            for b in self.ts_values:
-                GmA = 0
-                i = 0
-                for e in zeta:
-                    fk = self.angular_cutoff.cutf()
-                    fj = self.radial_cutoff.cutf()
-                    GmA = GmA + (((1 + math.cos(e - b))) ** l) * \
-                          math.exp(- n * ((((Rj[i] + Rk[i]) / 2) - a) ** 2)) * fj * fk
-                    i = i + 1
-                GmA = GmA * (2 ** (1 - l))
-                GmAs.append(GmA)
-        return GmAs
+        AEVs = radial_aev_class()
+        for atom1 in self.hierarchy.atoms():
+            x = str(atom1.i_seq)
+            a = atom1.element.upper().strip()
+            AEVs.setdefault(a+x, {})
+            for b, atom2list in self.Atome_classify().items():
+                for c, atom3list in self.Atome_classify().items():
+                    for Rs in self.angular_rs_values:
+                        for zetas in self.ts_values:
+                            AEVs[a+x].setdefault(b+c, [])
+                            GmA = 0
+                            for atom2 in atom2list:
+                                for atom3 in atom3list:
+                                    if atom2 != atom1 and atom3 != atom1 and atom2 != atom3:
+                                        Rij = atom1.distance(atom2)
+                                        Rik = atom1.distance(atom2)
+                                        ZETAijk = atom1.angle(atom2, atom3)
+                                        fk = self.cutf(Rik)
+                                        fj = self.cutf(Rij)
+                                        GmA += (((1 + math.cos(ZETAijk - zetas))) ** l) * \
+                                              math.exp(- n * ((((Rij + Rik) / 2) - Rs) ** 2)) * fj * fk
+                            GmA = GmA * (2 ** (1 - l))
+                            AEVs[a+x][b+c].append(GmA)
+        return AEVs
--- test.py
+++ test.py
@@ -6,8 +6,10 @@ from AEVclass import Apart
 def main(pdb_file_name):
     #a = AEV(pdb_file_name)
     #print(a.Atome_classify())
-    b = Rpart(pdb_file_name)
-    print(b.R_AEV())
+    #b = Rpart(pdb_file_name)
+    #print(b.R_AEV())
+    c = Apart(pdb_file_name)
+    print(c.A_AEV())
 
 if __name__ == '__main__':
     import os, sys
