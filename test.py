be3c2fc2b8e92d89c67cd894490bd6fe71d02b31
Bob Wang <35225645+BobWangRobot@users.noreply.github.com>
2019-03-29 22:39:47 +0800

    Update test.py
    delect some redundancy

--- test.py
+++ test.py
@@ -19,25 +19,8 @@ def main(pdb_file_name1,pdb_file_name2):
     # print(a.get_AEVS(), b.get_AEVS())
     f = compare(a, b)
     print(f)
-    # print(a.Rpart())
-    # print(a.Apart())
-    # print(a.get_AEVS())
-
-
 
 
 if __name__ == '__main__':
     import os, sys
     main(*tuple(sys.argv[1:]))
-#
-# #a = AEV('acid060.pdb')
-# #print(a.Atome_classify())
-# #b = Rpart('acid060.pdb')
-# #print(b.R_AEV())
-# #c = Apart('acid060.pdb')
-# #print(c.Aeq())
-#
-# a = AEV(pdb_file_name='acid060.pdb')
-# b = AEV(pdb_file_name='acid300.pdb')
-# def compare(dic1, dic2):
-
