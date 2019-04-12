879d4577b0d616c5c883832bca7779bf123b7c97
Bob <Bob>
2019-04-12 16:06:13 +0800

    optimazeadd figure title and fix some bug

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ def compare(aev1, aev2):
   for element, values in aev1.get_AEVS().items():
     for r_or_a, value in values.items():
       if 0 in value:
-        diff[element][r_or_a] = (0, 1)
+        diff[element][r_or_a] = (1, 1)
       else:
         covalue = list(np.corrcoef(value, diff[element][r_or_a]))
         diff[element][r_or_a] = covalue[1]
@@ -30,30 +30,38 @@ def plotvalue(aev1, aev2, elename):
         for list2 in value:
           y2.append(list2)
   x = range(len(y1))
+  plt.title("AEV difference with two %s atom" % elename)
+  plt.xlabel("radial or angular of %s atom" % elename)
+  plt.ylabel("value")
   plt.plot(x, y1, 'r')
   plt.plot(x, y2, 'g')
   plt.xticks(x[::8], name)
   plt.show()
 
-def plotcompare(diff):
+def plotcompare(diff,ele):
   name = []
   y1 = []
   y2 = []
-  for atoms, values in diff.items():
+  for element, values in diff.items():
+    if ele in element:
       for r_or_a, value in values.items():
         name.append(r_or_a)
         y1.append(value[0])
         y2.append(value[1])
   x = range(len(y1))
-  plt.scatter(x, y1)
-  plt.plot(x, y2)
+  plt.title("Correlation coefficient of %s atom"%ele)
+  plt.xlabel("radial or angular to %s"%ele)
+  plt.ylabel("value")
+  plt.plot(x, y1, 'ob')
+  plt.plot(x, y2, 'r')
   plt.xticks(x, name)
   plt.show()
 
 def main(pdb_file_name1, pdb_file_name2, element):
   a = AEV(pdb_file_name1)
   b = AEV(pdb_file_name2)
-  plotcompare(compare(a, b))
+  print(a.get_AEVS(), b.get_AEVS())
+  plotcompare(compare(a, b), element)
   plotvalue(a, b, element)
 
 
